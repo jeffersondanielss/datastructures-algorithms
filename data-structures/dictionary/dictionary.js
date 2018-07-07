51af4cb95008655cb3d9740c183c9684582f5114
Jefferson Daniel <jeffersondanielss@gmail.com>
2018-07-07 09:35:48 -0300

    feat: all methods in the dictionary data structure

--- data-structures/dictionary/dictionary.js
+++ data-structures/dictionary/dictionary.js
@@ -1,12 +1,116 @@
 function Dictionary() {
   let items = {};
+  
+  /**
+   * Check if key exists in dictionary
+   * 
+   * @param {any} key - item to be searched
+   * @returns {Boolean}
+   */
 
   this.has = function(key) {
     return key in items;
   }
 
+  /**
+   * Add new item in dictionary
+   * 
+   * @param {any} key - key of item to be add
+   * @param {any} value - value of item to be add
+   * @returns {void}
+   */
+
   this.set = function(key, value) {
     items[key] = value;
+  }
+
+  /**
+   * Remove a record in dictionary
+   * 
+   * @param {any} key - key of element to be deleted
+   * @returns {Boolean}
+   */
+
+  this.delete = function(key) {
+    if(this.has(key)) {
+      delete items[key];
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * fetch a record in dictionary
+   * 
+   * @param {any} key 
+   * @returns {any}
+   */
+  this.get = function(key) {
+    return this.has(key) ? items[key] : undefined; 
+  }
+
+  /**
+   * Count number of items in dictionary
+   * 
+   * @returns {number}
+   */
+
+  this.size = function() {
+    let count = 0;
+
+    for( let key in items ) {
+      if( items.hasOwnProperty(key) ) {
+        count++
+      }
+    }
+
+    return count
+  }
+
+  /**
+   * Remove all itens
+   * 
+   * @returns {void}
+   */
+  
+  this.clear = function() {
+    items = {}
+  }
+
+  /**
+   * Get all keys of items in dictionary
+   * 
+   * @returns {array}
+   */
+
+  this.keys = function() {
+    return Object.keys(items);
+  }
+
+  /**
+   * Fetch all itens
+   */
+
+  this.getItems = function() {
+    return items;
+  }
+
+  /**
+   * Get all values in dictionary
+   * 
+   * @returns {array}
+   */
+
+  this.values = function() {
+    var values = [];
+
+    for( var item in items ) {
+      if( this.has(item) ) {
+        values.push(items[item]);
+      }
+    }
+
+    return values;
   }
 }
 
--- data-structures/dictionary/test.js
+++ data-structures/dictionary/test.js
@@ -2,6 +2,14 @@ const Dictionary = require('./dictionary');
 
 const dictionary = new Dictionary();
 
+beforeEach(() => {
+  dictionary.set('Brazil', 'Hexa');
+  dictionary.set('World cup', '2018');
+});
+
+afterEach(() => {
+  dictionary.clear();
+})
 
 test('Dictionary is a class', () => {
   expect(() => new Dictionary()).not.toThrow();
@@ -11,15 +19,56 @@ test('has function exists', () => {
   expect(() => dictionary.has('test')).not.toThrow();
 });
 
-test('set function exists', () => {
-  expect(() => dictionary.set('key', 'value')).not.toThrow();
-});
-
 test('has check non exists element', () => {
   expect(dictionary.has('Belgium')).toBeFalsy();
 });
 
 test('has check exist element', () => {
-  dictionary.set('Brazil', 'hexa');
   expect(dictionary.has('Brazil')).toBeTruthy();
+});
+
+test('set function exists', () => {
+  expect(() => dictionary.set('key', 'value')).not.toThrow();
+});
+
+test('delete function exists', () => {
+  expect(() => dictionary.delete('non exists')).not.toThrow();
+});
+
+test('shound remove a item in dictionary', () => {
+  expect(dictionary.delete('Brazil')).toBeTruthy();
+});
+
+test('Remove a non exist item in dictionary', () => {
+  expect(dictionary.delete('non exixtis')).toBeFalsy();
+});
+
+test('get function exists', () => {
+  expect(() => dictionary.get('non exists')).not.toThrow();
+});
+
+test('search item', () => {
+  expect(dictionary.get('Brazil')).toEqual('Hexa');
+})
+
+test('shound get number of items', () => {
+  expect(dictionary.size()).toEqual(2);
+});
+
+test('clear shound remove all itens in dictionary', () => {
+  dictionary.clear()
+
+  expect(dictionary.size()).toEqual(0);
+});
+
+test('keys shound get all keys in dictionary', () => {
+  expect(dictionary.keys()).toEqual(['Brazil', 'World cup']);
+});
+
+test('get all items', () => {
+  expect(dictionary.getItems()).toEqual({'Brazil': 'Hexa', 'World cup': '2018'});
+});
+
+test('get all values in dictionary', () => {
+  expect(dictionary.values()).toEqual(['Hexa', '2018']);
 });
